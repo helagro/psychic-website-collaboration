bb37248458b1788de36e9765cb7faa1ccb942966
Henrik Lagrosen <henrikl@tuta.io>
2020-11-05 16:48:13 +0100

    card generation work among other things

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const denos = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+const cardImages = ["hearts", "spades", "diamonds", "clubs"]
+const isDbug = true
+
+function main(){
+    addCards()
+}
+
+function addCards(){
+    cardJSONs = []
+
+    for (let i = 0; i < 6; i++){
+        card = generateRandomCard()
+        cardJSON = JSON.stringify(card)
+
+        if(cardJSONs.includes(cardJSON)){ //Prevents same card from appearing twice
+            i--
+            continue
+        }
+
+        cardJSONs.push(cardJSON)
+        console.log(cardJSON)
+    }
+}
+
+function generateRandomCard(){
+    const deno = getRandomElement(denos)
+    const img = getRandomElement(cardImages)
+
+    return {deno, img}
+}
+
+function getRandomElement(array){
+    const randomNumInRange = Math.floor(Math.random()*array.length)
+    return array[randomNumInRange]
+}
+
+
+main()
